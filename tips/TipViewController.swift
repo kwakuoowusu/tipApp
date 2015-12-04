cbef282b7fd7c8ea5fd4021b9f81fbef966a0ee3
kwakuoowusu <kwakuoowusu@gmail.com>
2015-12-04 01:11:12 -0500

    updated name of view controller

--- tips/TipViewController.swift
+++ tips/TipViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TipViewController.swift
 //  tips
 //
 //  Created by Kwaku Owusu on 12/1/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
 
     
     @IBOutlet weak var billField: UITextField!
